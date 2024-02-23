5ab3fa506b918072b8726d8fe5e8a1f794bc750c
Dih Paraguai <diego.paraguai.dp@gmail.com>
2024-02-23 18:59:25 -0300

    iniciando projeto de consulta de personagens de harry potter

---
+++ HogwartsApi/Modelos/Personagem.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HogwartsApi.Modelos;
+
+internal class Personagem
+{
+    [JsonPropertyName("name")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("gender")]
+    public string? sexo { private get; set; }
+    public string? Sexo
+    {
+        get
+        { 
+            return (sexo == "male") ? "Masculino": "Feminino";
+        } 
+    }
+    [JsonPropertyName("house")]
+    public string? Casa { get; set; }
+
+    internal void ExibirDetalhes()
+    {
+        Console.WriteLine($@"
+Nome do Personagem: {Nome}
+Sexo: {Sexo}
+Casa: {Casa}
+");
+    }
+}
---
+++ HogwartsApi/Program.cs
@@ -0,0 +1,13 @@
+﻿using HogwartsApi.Modelos;
+using System.Text.Json;
+List<Personagem> personagens = new(); 
+using (HttpClient client = new HttpClient())
+{
+    string res = await client.GetStringAsync("https://hp-api.onrender.com/api/characters/house/gryffindor");
+    personagens = JsonSerializer.Deserialize<List<Personagem>>(res)!;
+}
+
+foreach (var item in personagens)
+{
+    item.ExibirDetalhes();
+}
